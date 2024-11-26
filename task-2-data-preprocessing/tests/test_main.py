ad56d5633bf0f88f075a7c5fb0ad7e3716dbdbcb
Edgar454 <mevaed4@gmail.com>
2024-11-26 11:49:39 +0100

    bug fixes

--- task-2-data-preprocessing/tests/test_main.py
+++ task-2-data-preprocessing/tests/test_main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'sri_lanka_chapter_')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'preprocessing_pipeline')))
 
 import asyncio
 import unittest
